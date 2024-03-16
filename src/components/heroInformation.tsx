6e688cfd0962b004842ea58e8f5d7ee76ca3c95c
safestub <admin@safestub.com>
2024-03-16 18:48:30 -0500

    Initial and possibly final commit

---
+++ src/components/heroInformation.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+
+
+interface HeroInformationContainerProps {
+
+}
+
+const className = "text-lg font-semibold flex flex-row"
+
+const InfoText = ({ content }: { content: string }) => {
+    return <p >{content}</p>
+}
+
+const HeroInformationContainer = (props: HeroInformationContainerProps) => {
+    return (
+        <div className={"flex flex-col justify-center items-center gap-2 [&_*]:uppercase"}>
+            <a
+                href="tel:12192661203"
+                className={`${className} flex flex-row group/textItem`}
+            >
+                <span
+                    className={"w-[2px] h-full mr-2 bg-transparent transition-colors duration-150 group-hover/textItem:bg-sky-500"}
+                />
+                <span>{"Direct #: (219) 266-1203"}</span>
+            </a>
+            <a
+                href="mailto:iskura@speedyrecoverytowing.com"
+                className={`${className} flex flex-row group/textItem`}
+            >
+                <span
+                    className={"w-[2px] h-full mr-2 bg-transparent transition-colors duration-150 group-hover/textItem:bg-sky-500"}
+                />
+                <span>
+                    {"Email: ISKURA@speedyrecoverytowing.com"}
+                </span>
+            </a>
+            <InfoText content="10420 S. Circle Drive" />
+            <InfoText content="Oak Lawn, Illinois 60453" />
+        </div>
+    )
+}
+
+
+HeroInformationContainer.displayName = "HeroInformationContainer"
+
+
+export default HeroInformationContainer;
---
+++ src/components/heroTitleContainer.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Image from "next/image";
+import heroImage from "../assets/hero.jpg"
+
+
+interface HeroTitleContainerProps {
+
+}
+
+const TitleText = ({ children }: { children: string }) => {
+    return (
+        <h1 className={"text-[7vw] lg:text-6xl font-bold whitespace-nowrap"}>{children}</h1>
+    )
+}
+
+const HeroTitleContainer = (props: HeroTitleContainerProps) => {
+    return (
+        <div className={"flex flex-col justify-center items-center gap-3"}>
+            <TitleText>Speedy Recovery</TitleText>
+            <Image
+                src={heroImage}
+                alt="Trucks aligned and ready to help"
+                fill
+                className={"!relative max-w-[min(768px,90vw)] lg:max-w-[35vw] h-auto"}
+            />
+            <TitleText>And Towing</TitleText>
+            <p className={"text-sm text-gray-400"}>A limited libability company</p>
+        </div>
+    )
+}
+
+
+HeroTitleContainer.displayName = "HeroTitleContainer"
+
+
+export default HeroTitleContainer;
